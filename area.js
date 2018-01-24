b4ee4dea165c3d8bb0785bf7e83ccde29430666a
Elvisnando <elvis_work@hotmail.it>
2018-01-24 16:06:39 +0100

    presume che funzione area lavora

--- area.js
+++ area.js
@@ -1,7 +1,19 @@
 function getArea(arr) {
     if (Array.isArray(arr) && arr.length == 2) {
+        if(Number.isInteger(arr[0])  &&  Number.isInteger(arr[1])) {
+            var result = arr[0] *arr[1];
+            if(result>= 0){
+                return result;
+            } else {
+                return -1;
+            }
 
+        } else {
+            return -1;
+        }
     
+    } else {
+        return -1;
     }
 
 
